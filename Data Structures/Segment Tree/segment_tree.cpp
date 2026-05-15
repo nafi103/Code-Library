f89995e269d946a34d1f0bab541e0c894f4d75c2
Nasif Shahrier <nasifshahrierrifat@gmail.com>
2026-05-15 23:53:49 +0600

    Cleaned up, applied references

--- Data Structures/Segment Tree/segment_tree.cpp
+++ Data Structures/Segment Tree/segment_tree.cpp
@@ -3,40 +3,40 @@
  * Time: O(N) build, O(log N) update/query
  * Use: Range Sum, Range Min/Max, Range GCD
  */
-struct Node {
-    int value;
-    Node(int val = 0) : value(val) {} 
-    // Default values: sum -> 0, min -> inf, max -> -inf
-};
-
-Node merge(Node &left, Node &right) {
-    return Node(left.value + right.value);
-}
 
 struct Segment_Tree {
-    int n;
-    vector<int> v;
-    vector<Node> st;
 
-    Segment_Tree(vector<int> &_v, int _n) {
-        n = _n;
-        st.resize(4 * n);
-        v = _v;
-        build(1, 1, n);
+    struct Node {
+        int value;
+        Node(int val = 0) : value(val) {} 
+        // Default values: sum -> 0, min -> inf, max -> -inf
+    };
+
+    Node merge(const Node &left, const Node &right) {
+        return Node(left.value + right.value);
     }
 
-    void build(int node, int b, int e) {
+    int n;
+    vector<Node> st;
+
+    Segment_Tree(const vector<int> &v, int _n) {
+        n = _n;
+        st.resize(4 * n + 1);
+        build(1, 1, n, v);
+    }
+
+    void build(int node, int b, int e, const vector<int> &v) {
         if (b == e) {
             st[node] = Node(v[b]);
             return;
         }
         int mid = (b + e) / 2, left = 2 * node, right = 2 * node + 1;
-        build(left, b, mid);
-        build(right, mid + 1, e);
+        build(left, b, mid, v);
+        build(right, mid + 1, e, v);
         st[node] = merge(st[left], st[right]);
     }
 
-    void update(int node, int b, int e, int idx, Node value) {
+    void update(int node, int b, int e, int idx, const Node &value) {
         if (e < idx or b > idx) return;
         if (b == idx and e == idx) {
             st[node] = value;
@@ -55,5 +55,14 @@ struct Segment_Tree {
         Node query_left = query(left, b, mid, l, r);
         Node query_right = query(right, mid + 1, e, l, r);
         return merge(query_left, query_right);
+    }
+
+    int query(int l, int r){
+        return query(1, 1, n, l, r).value;
+    }
+
+    void update(int idx, int value){
+        Node tmp = Node(value);
+        update(1, 1, n, idx, tmp);
     }
 };
